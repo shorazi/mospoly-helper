c485ccebe34fd10b89aed7ecf567277440af6627
Shorazi B. Azizov <ShoraziA@HUMO.local>
2024-11-27 17:08:43 +0500

    update: sass to tailwind in layout;

--- src/widgets/layout/ui/layout.content.tsx
+++ src/widgets/layout/ui/layout.content.tsx
@@ -7,20 +7,30 @@ const Layout = (
 ) => {
 	return (
 		<div className={styles.root}>
-			<div className={styles.container + ' bg-slate-800'}>
-				<div className={styles.outlet}>{props.children}</div>
+			<div className='max-w-md w-full min-h-dvh flex flex-col relative bg-slate-800'>
+				<div className='w-full grid grid-cols-1'>{props.children}</div>
 			</div>
 			<MobileBar />
 		</div>
 	)
 }
+// return (
+// 	<div className={styles.root}>
+// 		<div className={styles.container + ' bg-slate-800'}>
+// 			<div className={styles.outlet}>{props.children}</div>
+// 		</div>
+// 		<MobileBar />
+// 	</div>
+// )
 
 export default Layout
 
 const MobileBar = () => (
-	<div className={styles.mobile_bar + ' bg-slate-700'}>
+	<div className='max-w-md w-full fixed self-end px-7 h-12 flex justify-between items-center bg-slate-700'>
 		<button>Главная</button>
 		<button>Профиль</button>
 		<button>Еще</button>
 	</div>
 )
+// <div className={styles.mobile_bar + ' bg-slate-700'}>
+// </div>
